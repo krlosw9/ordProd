81c74b3f71fef12001a097536e488f33779a5de6
krlos waldo r <krlosw9@gmail.com>
2019-02-01 18:32:06 -0500

    Se elimino Project Printable y BaseElement.php de los modelos, estos archivos no van con el fin de este proyecto..:)

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,17 +2,12 @@
 
 namespace App\Controllers;
 
-use App\Models\{Job,Project};
+use App\Models\Job;
 
 class IndexController extends BaseController{
 	public function indexAction(){
 
 		$jobs = Job::all();
-
-		$project1 = new Project('Project 1','Description 1');
-		$projects = [
-		  $project1
-		];
 
 		$nombre = 'Carlos Waldo';
 		$limitsMonths = 2000;
