8d41166d0525288b34112d114f6b6e0334280408
Carlos Waldo <krlosw9@gmail.com>
2019-10-26 17:30:20 -0500

    Modificado la salida errorPhp

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors',0);
 ini_set('display_starup_error',0);
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
 require_once './mapRoute.php';
--- public/mapRoute.php
+++ public/mapRoute.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors',1);
 ini_set('display_starup_error',1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_PARSE); 
 
 //require_once '../vendor/autoload.php';
 use Aura\Router\RouterContainer;
