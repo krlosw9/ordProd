ca4985136fa8d3eedc4de9594674b9fe57eba922
Carlos Waldo <krlosw9@gmail.com>
2019-11-06 23:25:22 -0500

    Modificado la pagina de inicio

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,14 +2,30 @@
 
 namespace App\Controllers;
 
-use App\Models\ActividadTarea;
+use App\Models\{ActividadTarea,InfoOrdenProduccion, Clientes, Pedido, ModelosInfo};
 
 class IndexController extends BaseController{
 	public function indexAction(){
+		
+		$queryOrden = InfoOrdenProduccion::all();
+		$cantOrdenes = $queryOrden->count();
+
+		$queryClientes = Clientes::where("tipo","=",0)->get();
+		$cantClientes = $queryClientes->count();
+
+		$queryPedidos = Pedido::all();
+		$cantPedidos = $queryPedidos->count();
+
+		$queryModelos = ModelosInfo::all();
+		$cantModelos = $queryModelos->count();
 
 		$valorX='100';
 		$year='2006';
 		return $this->renderHTML('index.twig', [
+			'cantOrdenes' => $cantOrdenes,
+			'cantClientes' => $cantClientes,
+			'cantPedidos' => $cantPedidos,
+			'cantModelos' => $cantModelos,
 			'valorX' => $valorX,
 			'year' => $year
 		]);
